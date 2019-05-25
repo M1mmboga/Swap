4916a2dee67a478e07a7ea29ce5735613e693733
johnGachihi <John.Waithaka@O365.strathmore.edu>
2019-05-26 00:11:20 +0300

    Worked on Goods listing look

--- app/src/main/java/com/johngachihi/swap/adapters/GoodsListAdapter.java
+++ app/src/main/java/com/johngachihi/swap/adapters/GoodsListAdapter.java
@@ -1,7 +1,10 @@
 package com.johngachihi.swap.adapters;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,14 +14,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.johngachihi.swap.R;
 import com.johngachihi.swap.models.Good;
+import com.johngachihi.swap.rest.constants.Addresses;
+import com.squareup.picasso.Picasso;
 
 public class GoodsListAdapter extends PagedListAdapter<Good, RecyclerView.ViewHolder> {
     private static final int ITEM_GOOD = 0;
     private static final int ITEM_PLACEHOLDER = 1;
 
+    private Context context;
 
-    public GoodsListAdapter(@NonNull DiffUtil.ItemCallback<Good> diffCallback) {
+    public GoodsListAdapter(@NonNull DiffUtil.ItemCallback<Good> diffCallback, Context context) {
         super(diffCallback);
+        this.context = context;
     }
 
     @NonNull
@@ -35,7 +42,15 @@ public class GoodsListAdapter extends PagedListAdapter<Good, RecyclerView.ViewHo
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         if(getItemViewType(position) == ITEM_GOOD) {
             Good good = getItem(position);
+            ImageView goodImage = ((GoodItemViewHolder)holder).goodImage;
+            Picasso.get().load(Addresses.IMAGES_HOME + good.getImageFileName()).into(goodImage);
             ((GoodItemViewHolder)holder).goodNameTxt.setText(good.getName());
+            ((GoodItemViewHolder) holder).goodDescriptionTxt.setText(good.getDescription());
+            Log.d("Offerer", good.getOfferer().getEmail());
+            ((GoodItemViewHolder) holder).goodPriceRangeTxt.setText(
+                    this.context.getString(R.string.good_price_range, good.getPriceRangeMin(), good.getPriceRangeMax()));
+            ((GoodItemViewHolder) holder).goodCategoryTxt.setText(
+                    context.getString(R.string.goods_list_category_label, good.getCategory()));
         }
     }
 
@@ -45,11 +60,19 @@ public class GoodsListAdapter extends PagedListAdapter<Good, RecyclerView.ViewHo
     }
 
     class GoodItemViewHolder extends RecyclerView.ViewHolder {
+        public ImageView goodImage;
         public TextView goodNameTxt;
+        public TextView goodDescriptionTxt;
+        public TextView goodPriceRangeTxt;
+        public TextView goodCategoryTxt;
 
         public GoodItemViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.goods_list_item, parent, false));
+            goodImage = (ImageView) itemView.findViewById(R.id.good_image);
             goodNameTxt = (TextView) itemView.findViewById(R.id.good_name_text);
+            goodDescriptionTxt = (TextView) itemView.findViewById(R.id.good_description);
+            goodPriceRangeTxt = (TextView) itemView.findViewById(R.id.good_price_range);
+            goodCategoryTxt = (TextView) itemView.findViewById(R.id.temp_good_category);
         }
     }
 
